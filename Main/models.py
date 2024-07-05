f23aec506056b720c61f4d675f0d46c22256fd0e
RJ <rj_cm@icloud.com>
2024-07-05 18:25:05 +0800

    update

--- Main/models.py
+++ Main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class AboutMe(models.Model):
-
     name = models.CharField(max_length=100, default='Ryan')
     college = models.CharField( max_length=100, default='Marinduque State College')
     description = models.TextField()
