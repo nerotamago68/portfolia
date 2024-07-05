d5eebbe328027508aa28e1d33dab2cf24312ae71
RJ <rj_cm@icloud.com>
2024-07-05 21:08:13 +0800

    update

--- build.sh
+++ build.sh
@@ -3,18 +3,13 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 
-python manage.py makemigrations
+python3 manage.py makemigrations
 
-python manage.py migrate
-
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+python3 manage.py migrate
